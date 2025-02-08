435cc5b4454e5b856fb037f11b8a6bfbca073f4e
Armando Pratama <armandopratama0524@gmail.com>
2025-02-08 14:19:37 +0700

    reactive data pada vue js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+var product = {
+    "name": "Bamboo Thermal Ski Coat",
+    "description": "You'll be the most environmentally conscious",
+    "price": "55",
+    "image": "https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg"
+}
+
+
 var app = new Vue ({
     el: '#app',
-    data: {
-        name: 'Vue.js'
-    }
+    data: product
 });
